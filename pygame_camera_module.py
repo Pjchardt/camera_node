9de92007fe15faec3455d4cc274bf5a52664eac6
pjchardt <richiehoagland@gmail.com>
2019-09-13 14:10:42 -0400

    added roation and scale to camera capture

--- pygame_camera_module.py
+++ pygame_camera_module.py
@@ -11,10 +11,13 @@ class PygameCameraModule(object):
     def start(self):
         pygame.camera.init()
         cameras = pygame.camera.list_cameras() #Camera detected or not
+        print(cameras)
         print ("Using camera %s ..." % cameras[0])
-        self.cam = pygame.camera.Camera(cameras[0], (1280, 720))
+        self.cam = pygame.camera.Camera(cameras[0], (640, 480))
         self.cam.start()
 
     def capture_image(self, name):
         img = self.cam.get_image()
+        img = pygame.transform.rotate(img, -90)
+        img = pygame.transform.smoothscale(img, (960, 1280))
         pygame.image.save(img,name)
